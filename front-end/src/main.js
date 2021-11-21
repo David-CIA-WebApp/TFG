d04c208cf76274f905908ebd34a04a87003f4e2e
davbrican <72500941+davbrican@users.noreply.github.com>
2021-11-21 18:21:22 +0100

    Update main.js

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import VueRouter from "vue-router";
 import Routes from "./routes";
 
+process.env.VUE_APP_BACK_URL = "http://141.94.203.217:8001";
+
 Vue.use(VueRouter);
 
 const rutas = new VueRouter({
