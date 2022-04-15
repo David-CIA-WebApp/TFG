0f1db596642cd17a536743811d930bed8c6b9065
davbrican <72500941+davbrican@users.noreply.github.com>
2022-04-15 02:21:08 +0200

    Implementar alertas

---
+++ back-end/dump-tfg.sql
@@ -0,0 +1,357 @@
+-- MySQL dump 10.13  Distrib 5.5.62, for Win64 (AMD64)
+--
+-- Host: localhost    Database: tfg
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.4.22-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `alertas`
+--
+
+DROP TABLE IF EXISTS `alertas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `alertas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(500) DEFAULT NULL,
+  `fecha` date DEFAULT NULL,
+  `tipoAlerta` enum('Revisión','Contacto','Inventario') DEFAULT NULL,
+  `activa` tinyint(1) DEFAULT NULL,
+  `id_access` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `alertas`
+--
+
+LOCK TABLES `alertas` WRITE;
+/*!40000 ALTER TABLE `alertas` DISABLE KEYS */;
+INSERT INTO `alertas` VALUES (1,'El trabajo nºX se realizó hace más de 5 años y necesita revisión periódica','2022-03-27','Revisión',1,'X'),(2,'Un nuevo usuario con correo X te ha contactado desde la web','2022-03-26','Contacto',1,'X'),(3,'Un nuevo usuario con correo X te ha contactado desde la web','2022-03-24','Contacto',1,'X'),(4,'Un nuevo usuario con correo X te ha contactado desde la web','2022-03-27','Contacto',1,'X'),(5,'Queda poca cantidad de X que debería ser repuesto','2022-03-26','Inventario',1,'X'),(9,'Queda poca cantidad de X que debería ser repuesto','2022-03-31','Inventario',0,'Tubo'),(10,'Queda poca cantidad de X que debería ser repuesto','2022-03-31','Inventario',0,'Tuerca 10mm'),(12,'Un nuevo usuario con correo X te ha contactado desde la web','2022-04-14','Contacto',0,'david.brincau@hotmail.com'),(13,'Un nuevo usuario con correo X te ha contactado desde la web','2022-04-14','Contacto',1,'correodeprueba@us.es');
+/*!40000 ALTER TABLE `alertas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `certificados`
+--
+
+DROP TABLE IF EXISTS `certificados`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `certificados` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ruta` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `certificados`
+--
+
+LOCK TABLES `certificados` WRITE;
+/*!40000 ALTER TABLE `certificados` DISABLE KEYS */;
+/*!40000 ALTER TABLE `certificados` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cita`
+--
+
+DROP TABLE IF EXISTS `cita`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cita` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_trabajo` int(11) DEFAULT NULL,
+  `fecha` datetime NOT NULL,
+  `certificado` int(11) DEFAULT NULL,
+  `id_tecnico` int(11) DEFAULT NULL,
+  `id_perito` int(11) DEFAULT NULL,
+  `id_administrador` int(11) DEFAULT NULL,
+  `descripcion` varchar(100) DEFAULT NULL,
+  `direccion` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cita`
+--
+
+LOCK TABLES `cita` WRITE;
+/*!40000 ALTER TABLE `cita` DISABLE KEYS */;
+INSERT INTO `cita` VALUES (1,1,'2022-02-11 09:10:00',NULL,NULL,4,NULL,'Revisión final','Calle Japon 2 6A'),(2,1,'2022-02-07 10:00:00',NULL,1,NULL,NULL,'Peritaje del trabajo','Calle Japon 2 6A'),(3,1,'2022-02-09 09:00:00',NULL,1,NULL,NULL,'Reparación completa','Calle Japon 5 4A'),(4,NULL,'2022-02-15 09:00:00',NULL,1,NULL,NULL,'Peritaje nuevo','Calle de prueba'),(5,NULL,'2022-03-10 08:46:00',NULL,NULL,NULL,NULL,'Curva lactosa niña - planta 1','Hptal. Virgen Macarena');
+/*!40000 ALTER TABLE `cita` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `clients`
+--
+
+DROP TABLE IF EXISTS `clients`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `clients` (
+  `id_cliente` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `clientePotencial` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id_cliente`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `clients_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id_persona`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `clients`
+--
+
+LOCK TABLES `clients` WRITE;
+/*!40000 ALTER TABLE `clients` DISABLE KEYS */;
+INSERT INTO `clients` VALUES (1,11,0);
+/*!40000 ALTER TABLE `clients` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `consultas`
+--
+
+DROP TABLE IF EXISTS `consultas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `consultas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) DEFAULT NULL,
+  `correo` varchar(200) DEFAULT NULL,
+  `descripcion` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `consultas`
+--
+
+LOCK TABLES `consultas` WRITE;
+/*!40000 ALTER TABLE `consultas` DISABLE KEYS */;
+INSERT INTO `consultas` VALUES (1,'David','david.brincau@hotmail.com','Esto es una prueba'),(2,'David','david.brincau@hotmail.com','Esto es una prueba\n'),(3,'ajop','correodeprueba@us.es','ajnio');
+/*!40000 ALTER TABLE `consultas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `externalworkers`
+--
+
+DROP TABLE IF EXISTS `externalworkers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `externalworkers` (
+  `id_ext` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `ocupacion` text COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id_ext`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `externalworkers_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id_persona`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `externalworkers`
+--
+
+LOCK TABLES `externalworkers` WRITE;
+/*!40000 ALTER TABLE `externalworkers` DISABLE KEYS */;
+INSERT INTO `externalworkers` VALUES (1,7,'Ferretero');
+/*!40000 ALTER TABLE `externalworkers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `gestor`
+--
+
+DROP TABLE IF EXISTS `gestor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `gestor` (
+  `id_gestor` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`id_gestor`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `gestor_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id_persona`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `gestor`
+--
+
+LOCK TABLES `gestor` WRITE;
+/*!40000 ALTER TABLE `gestor` DISABLE KEYS */;
+INSERT INTO `gestor` VALUES (2,9),(3,12);
+/*!40000 ALTER TABLE `gestor` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `materials`
+--
+
+DROP TABLE IF EXISTS `materials`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `materials` (
+  `nombre` varchar(100) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `stockSeguridad` int(11) NOT NULL,
+  PRIMARY KEY (`nombre`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `materials`
+--
+
+LOCK TABLES `materials` WRITE;
+/*!40000 ALTER TABLE `materials` DISABLE KEYS */;
+INSERT INTO `materials` VALUES ('Arandelas',210,50),('Tubo',6,10),('Tuerca 10mm',4,5);
+/*!40000 ALTER TABLE `materials` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `proveedores`
+--
+
+DROP TABLE IF EXISTS `proveedores`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `proveedores` (
+  `nombre` varchar(100) DEFAULT NULL,
+  `email` varchar(250) DEFAULT NULL,
+  `direccion` varchar(500) DEFAULT NULL,
+  `telefono` varchar(12) DEFAULT NULL,
+  `id` smallint(6) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `proveedores`
+--
+
+LOCK TABLES `proveedores` WRITE;
+/*!40000 ALTER TABLE `proveedores` DISABLE KEYS */;
+INSERT INTO `proveedores` VALUES ('Bricomarkt',NULL,'Av. Príncipe de Asturias, s/n, 41500 Alcalá de Guadaíra, Sevilla','955 62 05 01',1),('Proveedor1','email1@hotmail.com','Calle de prueba 1','678 98 09 87',2),('Proveedor2','email@us.es','Calle de prueba 2','678 89 90 09',4);
+/*!40000 ALTER TABLE `proveedores` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `trabajo`
+--
+
+DROP TABLE IF EXISTS `trabajo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `trabajo` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `tipo` enum('Instalacion de agua','Instalacion de gas','Revision de agua','Revision de gas','Reparacion','otro') CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `descripcion` text NOT NULL,
+  `direccion` text NOT NULL,
+  `id_cliente` int(11) NOT NULL,
+  `certificado` text DEFAULT NULL,
+  `presupuesto` text DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `trabajo`
+--
+
+LOCK TABLES `trabajo` WRITE;
+/*!40000 ALTER TABLE `trabajo` DISABLE KEYS */;
+INSERT INTO `trabajo` VALUES (1,'Instalacion de agua','Instalación de agua de cocina completa con grifo y salida para patio de exterior','Calle Japon 5 4A',11,NULL,NULL),(2,'Revision de agua','Revisión de la instalación','Calle Japón 5 4A',11,NULL,NULL),(3,'Revision de agua','esto es una prueba','jander clander',11,NULL,NULL);
+/*!40000 ALTER TABLE `trabajo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id_persona` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` text COLLATE utf8_unicode_ci NOT NULL,
+  `apellidos` text COLLATE utf8_unicode_ci NOT NULL,
+  `dni` text COLLATE utf8_unicode_ci NOT NULL,
+  `email` text COLLATE utf8_unicode_ci NOT NULL,
+  `direccion` text COLLATE utf8_unicode_ci NOT NULL,
+  `telefono` bigint(20) NOT NULL,
+  PRIMARY KEY (`id_persona`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (3,'David','Brincau Cano','29506399K','david.brincau@hotmail.com','Calle vedra Nº1',678623620),(4,'Admin','Admin User','00000000X','admin@david&cia.com','SE',999999999),(7,'Jesus','Aparicio','25978463P','troapasej@us.es','Calle de test',601072573),(9,'Gestor nombre','apellidos gestor','28497845Ñ','gestor@david&cia.com','Avda Reina Mercedes 2 2ºB',678542985),(11,'Carmen','Muñoz Perez','28675432P','carmunper1@alum.us.es','Calle Japon 2 6A',673600485),(12,'Luis','Marin','24589678Ñ','sin mail','sin especificar',670123456);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `workers`
+--
+
+DROP TABLE IF EXISTS `workers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `workers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `pass` text COLLATE utf8_unicode_ci NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `tipo` enum('Administrador','Perito','Técnico') COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `workers_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id_persona`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `workers`
+--
+
+LOCK TABLES `workers` WRITE;
+/*!40000 ALTER TABLE `workers` DISABLE KEYS */;
+INSERT INTO `workers` VALUES (1,'0T234T6T897TcT11f5disKl21oñlPq17',3,'Técnico'),(2,'contraseña',4,'Administrador'),(4,'Peritisimo',3,'Perito');
+/*!40000 ALTER TABLE `workers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'tfg'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-04-15  1:59:52
--- check-jobs/index.js
+++ check-jobs/index.js
@@ -1,31 +1,57 @@
 const axios = require('axios');
 
 var trabajos = [];
-const path = `${process.env.VUE_APP_BACK_URL}/trabajos`;
+
+const path = `${process.env.VUE_APP_BACK_URL}/login`;
 const config = {
-    method: 'get',
+    method: 'post',
     url: path,
+    data: {
+        "mail": `${process.env.user}`, 
+        "password": `${process.env.password}`
+    },
     headers: {
         "Content-Type": "application/JSON",
-        "Access-Control-Allow-Origin": "*",
-        "Authorization": this.token
+        "Access-Control-Allow-Origin": "*"
     }
-}
-axios(config).then((res) => {
-    for (let index = 0; index < res.data.length; index++) {
-        var element = res.data[index];
-        trabajos[index] = element;
-    }
-    setInterval(function(){
-        var d = new Date();
-        var h = d.getHours();
-        var m = d.getMinutes();
-        var s = d.getSeconds();
-        if (h == 12 && m == 17 && s == 0) {
-            console.log(trabajos);
-        } else {
-            console.log(h + ":" + m + ":" + s);
-        }
-    }, 1000);
-});
+};
 
+
+setInterval(function(){
+    var d = new Date();
+    var h = d.getHours();
+    var m = d.getMinutes();
+    var s = d.getSeconds();
+    if (h == 12 && m == 0 && s == 0) {
+        axios(config)
+        .then((res) => {
+            if (res.data.accepted) { 
+                const path2 = `${process.env.VUE_APP_BACK_URL}/trabajos`;
+                const config2 = {
+                    method: 'get',
+                    url: path2,
+                    headers: {
+                        "Content-Type": "application/JSON",
+                        "Access-Control-Allow-Origin": "*",
+                        "Authorization": res.data.token
+                    }
+                }
+                axios(config2).then((res) => {
+                    for (let index = 0; index < res.data.length; index++) {
+                        var element = res.data[index];
+                        trabajos[index] = element;
+                    }
+                })
+                .catch((err) => {
+                    console.log("TError:",err);
+                });
+            }
+        })
+        .catch((err) => {
+            console.log("LError:",err);
+        });
+        console.log(trabajos);
+    } else {
+        console.log(h + ":" + m + ":" + s);
+    }
+}, 1000);
